245ea29635c33548ae45084129f3b6a83f980ac8
EnesAksoy <enesaksoy1168@gmail.com>
2019-07-17 17:21:08 +0300

    News view at the tableview(SOAP API)

---
+++ News/NewsDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewsDetailViewController.swift
+//  News
+//
+//  Created by ENES AKSOY on 10.07.2019.
+//  Copyright © 2019 ENES AKSOY. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
